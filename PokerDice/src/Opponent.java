8838eb3e10f87103c29f99c69f2649ddaa8b2272
seth <slschoming@gmail.com>
2013-04-23 16:53:18 -0500

    more opponent functions

--- PokerDice/src/Opponent.java
+++ PokerDice/src/Opponent.java
@@ -9,20 +9,65 @@ public class Opponent extends Player {
 		return strategy;
 	}
 	
-	public void goForStraight() {
+	public void goForStraight(int One) {
+		if (One == 1){
+			d1.roll();
+		}
+		
+		if(One == 2){
+			d2.roll();
+		}
+		
+		if(One == 3){
+			d3.roll();
+		}
+		
+		if(One == 4){
+			d4.roll();
+		}
+		
+		if(One == 5){
+			d5.roll();
+		}
+		
+	}
+	
+	public void goForPairs(int die1) {
 		
 		
 		
 	}
-	
-	public void goForFive() {
+	public void goForPairs(int die1, int die2) {
 		
 		
 		
 	}
-	
-	public void goForFullHouse() {
+
+	public void goForPairs(int die1, int die2, int die3) {
 		
+		
+		
+	}
+	public void goForFullHouse(int die) {
+		if (die == 1){
+			d1.roll();
+		}
+		
+		if(die == 2){
+			d2.roll();
+		}
+		
+		if(die == 3){
+			d3.roll();
+		}
+		
+		if(die == 4){
+			d4.roll();
+		}
+		
+		if(die == 5){
+			d5.roll();
+		}
 		
 		
 	}
